992884d59763562efcb2f4a4c4f191f0f1d52261
berno_4444 <ivankatrenko1997@gmail.com>
2024-02-14 23:31:44 +0100

    new file

---
+++ src/components/app/App.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import AppHeader from "../appHeader/AppHeader";
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+
+import decoration from '../../resources/img/vision.png';
+
+const App = () => {
+
+    const [selectedChar, setChar] = useState(null); //здесь деструктиризация массива
+
+    const onCharSelected = (id) => {
+        setChar(id);
+    }
+
+    return (
+        <div className="app">
+            <AppHeader />
+            <main>
+                {/* {this.state.showRandomChar ? <RandomChar /> : null}
+                {/* <button onClick={this.toggleRandomChar}>
+                        Click me
+                    </button> */}
+                <ErrorBoundary>
+                    <RandomChar />
+                </ErrorBoundary>
+                <div className="char__content">
+                    <CharList onCharSelected={onCharSelected} />
+                    <ErrorBoundary>
+                        <CharInfo charId={selectedChar} />
+                    </ErrorBoundary>
+                </div>
+                <img className="bg-decoration" src={decoration} alt="vision" />
+            </main>
+        </div>
+    )
+
+}
+
+export default App;
