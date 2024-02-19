6882df96328666c3e3debff2757f59fba7c24675
berno_4444 <ivankatrenko1997@gmail.com>
2024-02-19 23:13:43 +0100

    new file

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,14 +1,16 @@
 import { useState } from "react";
+
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import CharSearchForm from "../charSearchForm/CharSearchForm";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
 
 const MainPage = () => {
 
-    const [selectedChar, setChar] = useState(null); //здесь деструктиризация массива
+    const [selectedChar, setChar] = useState(null);
 
     const onCharSelected = (id) => {
         setChar(id);
@@ -16,14 +18,25 @@ const MainPage = () => {
 
     return (
         <>
-            <RandomChar />
+            <ErrorBoundary>
+                <RandomChar />
+            </ErrorBoundary>
             <div className="char__content">
-                <CharList onCharSelected={onCharSelected} />
-                <CharInfo charId={selectedChar} />
+                <ErrorBoundary>
+                    <CharList onCharSelected={onCharSelected} />
+                </ErrorBoundary>
+                <div>
+                    <ErrorBoundary>
+                        <CharInfo charId={selectedChar} />
+                    </ErrorBoundary>
+                    <ErrorBoundary>
+                        <CharSearchForm />
+                    </ErrorBoundary>
+                </div>
             </div>
             <img className="bg-decoration" src={decoration} alt="vision" />
         </>
     )
-
 }
+
 export default MainPage;
